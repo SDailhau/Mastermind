628122f5c6ab94bff756af6accffef4d328add2f
Lisa Laplace <llaplace@pcalb-mm0000.univ-jfc.fr>
2020-02-12 14:42:52 +0100

    Debut fenetre

--- src/Mastermind.java
+++ src/Mastermind.java
@@ -1,9 +1,28 @@
+import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.awt.LayoutManager;
 
-public class Mastermind {
+public class Mastermind extends Frame{
 
+	public Mastermind() {
+		super();
+		 
+		Modele m = new Modele();
+		
+		VueClavier vueClavier = new VueClavier();
+		
+		LayoutManager layout = new BorderLayout();
+		this.setLayout(layout);
+		this.add(vueClavier,BorderLayout.PAGE_END);
+		
+		this.setVisible(true);
+		this.setSize(400,720);
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Mastermind();
 	}
 
 }
---
+++ src/VueClavier.java
@@ -0,0 +1,20 @@
+import java.awt.Button;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Panel;
+import java.util.ArrayList;
+
+public class VueClavier extends Panel{
+	
+	public VueClavier() {
+		super();
+		this.setVisible(true);
+		this.setBackground(Color.MAGENTA);
+		
+		
+		
+		
+		
+	}
+	
+}
