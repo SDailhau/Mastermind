30498dad64c3b744897553a156d11f14625e1c24
Sandra Dailhau <sdailhau@pcalb-mm0000.univ-jfc.fr>
2020-02-12 14:07:13 +0100

    Commencement Modele

---
+++ src/Modele.java
@@ -0,0 +1,6 @@
+import java.awt.Color;
+
+public class Modele {
+	public static Color COULEURS[]= {Color.YELLOW, Color.GREEN, Color.BLUE, Color.MAGENTA, Color.RED, Color.ORANGE, Color.WHITE, Color.BLACK};
+	public static int N_TENTATIVES=10, DIFFICULTE=4;
+}
